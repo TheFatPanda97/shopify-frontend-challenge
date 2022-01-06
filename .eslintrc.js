1c51e0cb706df5f022b936b39a2409234e5bc038
TheFatPanda97 <shawnhu97@gmail.com>
2022-01-05 21:14:38 -0500

    fixing some eslint configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['plugin:react/recommended', 'airbnb'],
+  extends: ['plugin:react/recommended', 'airbnb', 'airbnb-typescript'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
@@ -11,9 +11,8 @@ module.exports = {
     },
     ecmaVersion: 12,
     sourceType: 'module',
+    project: './tsconfig.json',
   },
   plugins: ['react', '@typescript-eslint'],
-  rules: {
-    'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
-  },
+  rules: {},
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import './scss/index.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="test">
         <p>
           Edit
           <code>src/App.tsx</code>
